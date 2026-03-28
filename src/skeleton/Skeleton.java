a94f17977569e9d62be8deffe0bdfc6f9489d6c4
zseszti0 <zseszti42.3@gmail.com>
2026-03-28 17:25:01 +0100

    Logger question interface added

--- src/skeleton/Skeleton.java
+++ src/skeleton/Skeleton.java
@@ -1,5 +1,7 @@
 package skeleton;
 
+import java.util.List;
+
 public class Skeleton {
 
     public static void main(String[] args) {
@@ -7,4 +9,16 @@ public class Skeleton {
     }
 
     public static void log(String command){}
+
+    public static boolean askBoolQuestion(String question){
+        log(question);
+        return true;
+    }
+    public static int askListQuestion(String question, List<Object> options){
+        log(question);
+        for( int i = 0; i < options.size(); i++){
+            System.out.println(i + ": " + options.get(i));
+        }
+        return 0;
+    }
 }
