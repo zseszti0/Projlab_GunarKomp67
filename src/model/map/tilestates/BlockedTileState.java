696f61452ddee354099d742568909d554a8a128c
Zrinyi22 <zrinyigimnfotok22.23@gmail.com>
2026-03-29 16:03:05 +0200

    added comments to tilestates classes

--- src/model/map/tilestates/BlockedTileState.java
+++ src/model/map/tilestates/BlockedTileState.java
@@ -1,8 +1,20 @@
 package model.map.tilestates;
 
+/**
+ * A blokkolt (jarhatatlan) mezoallapot. Baleset eseten jon letre a savban.
+ * Ebben az allapotban semmilyen hoeses vagy olvadas nem valtoztatja meg az allapotot.
+ * Singleton tervezesi mintat hasznal.
+ */
 public class BlockedTileState extends TileState {
+
     private static BlockedTileState instance;
+
     private BlockedTileState() {}
+
+    /**
+     * Visszaadja a BlockedTileState egyetlen peldanyat.
+     * @return a BlockedTileState peldany
+     */
     public static BlockedTileState getInstance() {
         if (instance == null) {
             instance = new BlockedTileState();
@@ -10,11 +22,19 @@ public class BlockedTileState extends TileState {
         return instance;
     }
 
+    /**
+     * Kezeli a hoeses esemenyet. Blokkolt mezonel nem tortenik allapotvaltozas.
+     * @return onmaga (BlockedTileState)
+     */
     @Override
     public TileState snowFall() {
         return this;
     }
 
+    /**
+     * Kezeli a ho olvadasat. Blokkolt mezonel nem tortenik allapotvaltozas.
+     * @return onmaga (BlockedTileState)
+     */
     @Override
     public TileState snowMelt() {
         return this;
--- src/model/map/tilestates/IcyTileState.java
+++ src/model/map/tilestates/IcyTileState.java
@@ -2,10 +2,20 @@ package model.map.tilestates;
 
 import model.shop.attachements.IcebreakerHead;
 
+/**
+ * A jeges mezoallapot. Sekely hobol jarmu athaladasakor keletkezik, es csuszasveszelyes a rajta haladok szamara.
+ * Singleton tervezesi mintat hasznal.
+ */
 public class IcyTileState extends TileState {
 
     private static IcyTileState instance;
+
     private IcyTileState() {}
+
+    /**
+     * Visszaadja a IcyTileState egyetlen peldanyat.
+     * @return a IcyTileState peldany
+     */
     public static IcyTileState getInstance() {
         if (instance == null) {
             instance = new IcyTileState();
@@ -13,16 +23,30 @@ public class IcyTileState extends TileState {
         return instance;
     }
 
+    /**
+     * Kezeli a hoeses esemenyet. Jeges mezon a hoeses nem valtoztat az allapoton, jeges marad.
+     * @return onmaga (IcyTileState)
+     */
     @Override
     public TileState snowFall() {
         return this;
     }
 
+    /**
+     * Kezeli a ho (jeg) olvadasat. A jeg elolvadasaval a mezo tiszta allapotba kerul.
+     * @return tiszta mezoallapot (CleanTileState)
+     */
     @Override
     public TileState snowMelt() {
         return CleanTileState.getInstance();
     }
 
+    /**
+     * Kezeli a jegtoro fej (IcebreakerHead) altali tisztitast.
+     * A jegtoro feltori a jeget, ami igy sekely havassa valik.
+     * @param ih a tisztitast vegzo jegtoro fej
+     * @return sekely havas mezoallapot (ShallowSnowyTileState)
+     */
     @Override
     public TileState cleanedBy(IcebreakerHead ih) {
         return ShallowSnowyTileState.getInstance();
