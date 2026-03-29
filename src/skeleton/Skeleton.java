c853cfa456b5118b2f3a0dd45e4d236b2a2ab5d9
zseszti0 <zseszti42.3@gmail.com>
2026-03-29 14:11:56 +0200

    Skeleton tracer final version done.

--- src/skeleton/Skeleton.java
+++ src/skeleton/Skeleton.java
@@ -1,13 +1,9 @@
 package skeleton;
 
-import model.DummyTester;
-
-import java.util.List;
-
 public class Skeleton {
 
     public static void main(String[] args) {
-        DummyTester.askQuestion();
+
     }
 
     public static void log(String command){}
