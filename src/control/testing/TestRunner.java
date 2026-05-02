83e05b63998a1d7cf6c51597faa274172d8afa74
Wernsdörfer Albert <dorferal@gmail.com>
2026-05-02 16:02:16 +0200

    added test runners skeleton and bus tests

---
+++ src/control/testing/TestRunner.java
@@ -0,0 +1,7 @@
+package control.testing;
+
+/**
+ * Ez az osztály valósítja meg az összes teszt lefuttatását, felhasználja az IndividualTestRunner osztályt az egyes tesztek futtatására.
+ */
+public class TestRunner {
+}
